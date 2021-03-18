767a6268572414ddbb248cd56885f265383dac35
ylqp <809685505@qq.com>
2021-03-18 16:13:32 +0800

    1

--- src/components/editor.vue
+++ src/components/editor.vue
@@ -24,7 +24,29 @@ export default {
             // 上传文件接口（这个地址是我为了方便各位体验文件上传功能搭建的临时接口，请勿在生产环境使用！！！部署在国外的服务器，如果无法访问，请自备梯子）
             serverUrl: 'http://35.201.165.105:8000/controller.php',
             // UEditor 资源文件的存放路径，如果你使用的是 vue-cli 生成的项目，通常不需要设置该选项，vue-ueditor-wrap 会自动处理常见的情况，如果需要特殊配置，参考下方的常见问题2
-            UEDITOR_HOME_URL: '/UEditor/'
+            UEDITOR_HOME_URL: '/UEditor/',
+            toolbars: [
+                [
+                    "fullscreen",
+                    "source",
+                    "|",
+                    "bold",
+                    "italic",
+                    "underline",
+                    "|",
+                    "fontsize",
+                    "fontfamily",
+                    "|",
+                    "kityformula",
+                    "preview",
+                    "|",
+                    "justifyleft",
+                    "justifycenter",
+                    "justifyright",
+                    "justifyjustify",
+                    "|"
+                ]
+            ]
           }
       }
   }
